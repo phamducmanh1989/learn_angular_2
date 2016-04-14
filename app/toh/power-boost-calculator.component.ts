8b8bfe7e07c5e1908046c80588d012f60da7be96
phamducmanh <phamducmanh1989@gmail.com>
2016-04-14 11:36:20 +0700

    change name total new

--- app/toh/power-boost-calculator.component.ts
+++ app/toh/power-boost-calculator.component.ts
@@ -16,7 +16,7 @@ import {ExponentialStrengthPipe} from './exponential-strength.pipe';
                 <input type="text"  class="form-control" [(ngModel)]="factor">
             </div>
             <div class="from-group">
-                <label for="factor">Super Hero Powerz: </label>
+                <label for="factor">Super Hero Power new: </label>
                 <input type="text" readonly="readonly" class="form-control" value="{{power | exponentialStrength: factor}}">
             </div>
         </form>
